ed8fcc16a6ab90a7538ab2616d77fa8b4a68db85
guxiaoyu <guxiaoyu@babeltime.com>
2026-09-22 22:36:27 +0800

    feat: bundle rcodesign(iOS) so in-app signing works (build in CI, set RCODESIGN_PATH)

--- SideInjector/App.swift
+++ SideInjector/App.swift
@@ -4,6 +4,13 @@ import SwiftUI
 struct SideInjectorApp: App {
     init() {
         _ = RustBridge.shared // 注册日志回调
+        // 让 Rust 签名后端指向 App 内置的 rcodesign(iOS) 二进制
+        if let rc = Bundle.main.url(forResource: "rcodesign", withExtension: nil)?.path {
+            setenv("RCODESIGN_PATH", rc, 1)
+            LogStore.shared.append("已加载内置 rcodesign：\(rc)")
+        } else {
+            LogStore.shared.append("未找到内置 rcodesign，App 内重签将不可用")
+        }
     }
 
     var body: some Scene {
